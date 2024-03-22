017e9da055b495b4e5f309cd22012ad4e2732c6d
ckaiserk <156959925+ckaiserk@users.noreply.github.com>
2024-03-22 23:29:23 +0000

    Cambios2

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import { Container, Row, Col, Button} from "react-bootstrap";
+import Login from "@/components/Login";
+
+export default function Home() {
+  return (
+    <Container>
+      <Login/>
+    </Container>
+  );
+}
